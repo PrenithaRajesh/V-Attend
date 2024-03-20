ca45ac5e3bfc951f202587d02aacd4013858d607
precoder365 <prenitharajesh04@gmail.com>
2024-03-20 20:58:33 +0530

    Setup student registration page

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title='v-attend')
+
+st.header('Welcome to V-Attend')
+
+st.write('A simple attendance management system that allows you to easily manage your attendance.')
+
---
+++ pages/Registration.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title='Registration Form')
+st.header('Registration Form')
+
